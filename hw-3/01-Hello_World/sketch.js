f7f18898cbf6db5b05f2e1abae9d2ca6d546a668
Halton Stancil <46690749+HaltonStancil@users.noreply.github.com>
2019-01-30 10:23:13 -0700

    updated canvas with Hello World and background color

--- hw-3/01-Hello_World/sketch.js
+++ hw-3/01-Hello_World/sketch.js
@@ -1,3 +1,14 @@
+// Declaring a setup function
 function setup (){
-  ellipse(20, 20, 30, 30);
+// creating a canvas that is 1024 px wide, 768 px tall
+  createCanvas(1024,768);
+// coloring the backgroung red
+  background('red');
+}
+
+// Declaring the draw function
+function draw(){
+// printing the text "Hello World!"
+// Text will be positioned at
+  text("Hello World!", 500, 250 );
 }
